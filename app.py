0f22f3045f4b2e9a4ad1c12106c859c46614b81c
Andersen <actfire@gmail.com>
2020-12-08 12:01:45 +0800

    update

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(
+    __name__,
+    static_url_path='',
+    static_folder='static',
+    template_folder='template'
+)
+
+
+@app.route('/')
+def hello_world():
+    myName = 'Andersen'
+    return f'Hello, World! {myName}'
